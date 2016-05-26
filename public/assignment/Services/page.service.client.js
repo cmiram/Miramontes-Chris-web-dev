9a9b620eada96cb8f12749f5e36ad1b88b40781c
cmiram <nyyankeeschris@gmail.com>
2016-05-26 16:18:55 -0400

    finished adding and implementing the four service pages for page, user, website, and widget

---
+++ public/assignment/Services/page.service.client.js
@@ -0,0 +1,70 @@
+(function() {
+    angular
+        .module("WebAppMaker")
+        .factory("PageService", PageService);
+
+    function PageService() {
+        var pages = [
+            { "_id": "321", "name": "Post 1", "websiteId": "456" },
+            { "_id": "432", "name": "Post 2", "websiteId": "456" },
+            { "_id": "543", "name": "Post 3", "websiteId": "456" }
+        ];
+
+        var api = {
+            createPage: createPage,
+            findPageByWebsiteId: findPageByWebsiteId,
+            findPageById: findPageById,
+            updatePage: updatePage,
+            deletePage: deletePage
+        };
+        return api;
+
+        function createPage(websiteId, page) {
+            var newPage = {
+                _id: Date.prototype.getDate().valueOf(),
+                name: page,
+                websiteId: websiteId
+            }
+            pages.push(newPage);
+        }
+
+        function findPageByWebsiteId(websiteId) {
+            var result = [];
+            for (var i in pages) {
+                if(pages[i].websiteId === websiteId) {
+                    result.push(pages[i]);
+                }
+            }
+            return result;
+        }
+
+        function findPageById(pageId) {
+            for(var i in pages) {
+                if(pages[i]._id === pageId) {
+                    return pages[i];
+                }
+            }
+            return null;
+        }
+
+        function updatePage(pageId, page) {
+            for(var i in pages) {
+                if(pages[i]._id === pageId) {
+                    pages[i].name = page;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function deletePage(pageId) {
+            for(var i in pages) {
+                if(pages[i]._id === pageId) {
+                    pages.remove(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+})();
