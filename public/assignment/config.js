27d481cae3d34575d87f6f0ccfb2c29cf38374ba
cmiram <nyyankeeschris@gmail.com>
2016-05-26 14:24:55 -0400

    fixed linking in root index.html to navigate to login page of assignment index.html

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -6,45 +6,45 @@
     function Config($routeProvider) {
         $routeProvider
             .when("/login", {
-                templateUrl: "assignment/views/User/login.view.client.html",
+                templateUrl: "views/User/login.view.client.html",
                 controller: "LoginController",
                 controllerAs: "model"
             })
             .when("/register", {
-                templateUrl: "assignment/views/user/register.view.client.html"
+                templateUrl: "views/user/register.view.client.html"
             })
             .when("/profile/:id", {
-                templateUrl: "assignment/views/user/profile.view.client.html",
+                templateUrl: "views/user/profile.view.client.html",
                 controller: "ProfileController",
                 controllerAs: "model"
             })
             .when("/user/:userId/website", {
-                templateUrl: "assignment/views/website/website-list.view.client.html",
+                templateUrl: "views/website/website-list.view.client.html",
                 controller: "WebsiteListController",
                 controllerAs: "model"
             })
             .when("/user/:userId/website/:websiteId", {
-                templateUrl: "assignment/views/website/website-edit.view.client.html",
+                templateUrl: "views/website/website-edit.view.client.html",
                 controller: "EditWebsiteController",
                 controllerAs: "model"
             })
             .when("/user/:userId/website/:websiteId/page", {
-                templateUrl: "assignment/views/Pages/page-list.view.client.html"
+                templateUrl: "views/Pages/page-list.view.client.html"
             })
             .when("/user/:userId/website/:websiteId/page/new", {
-                templateUrl: "assignment/views/Pages/page-new.view.client.html"
+                templateUrl: "views/Pages/page-new.view.client.html"
             })
             .when("/user/:userId/website/:websiteId/page/:pid", {
-                templateUrl: "assignment/views/Pages/page-edit.view.client.html"
+                templateUrl: "views/Pages/page-edit.view.client.html"
             })
             .when("/user/:userId/website/:websiteId/page/:pid/widget", {
-                templateUrl: "assignment/views/Widget/widget-list.view.client.html"
+                templateUrl: "views/Widget/widget-list.view.client.html"
             })
             .when("/user/:userId/website/:websiteId/page/:pid/widget/new", {
-                templateUrl: "assignment/views/Widget/widget-chooser.view.client.html"
+                templateUrl: "views/Widget/widget-chooser.view.client.html"
             })
             .when("/user/:userId/website/:websiteId/page/:pid/widget/:wgid", {
-                templateUrl: "assignment/views/widget/widget-edit.view.client.html"
+                templateUrl: "views/widget/widget-edit.view.client.html"
             })
             .otherwise({
                 redirectTo: "/login"
