42496115df90a3491270043f06a42effb28ff3f6
cmiram <nyyankeeschris@gmail.com>
2016-05-26 16:34:12 -0400

    modified config to add controllers to each .when

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -11,7 +11,9 @@
                 controllerAs: "model"
             })
             .when("/register", {
-                templateUrl: "views/user/register.view.client.html"
+                templateUrl: "views/user/register.view.client.html",
+                controller: "RegisterController",
+                controllerAs: "model"
             })
             .when("/profile/:id", {
                 templateUrl: "views/user/profile.view.client.html",
@@ -29,22 +31,34 @@
                 controllerAs: "model"
             })
             .when("/user/:userId/website/:websiteId/page", {
-                templateUrl: "views/Pages/page-list.view.client.html"
+                templateUrl: "views/Pages/page-list.view.client.html",
+                controller: "PageListController",
+                controllerAs: "model"
             })
             .when("/user/:userId/website/:websiteId/page/new", {
-                templateUrl: "views/Pages/page-new.view.client.html"
+                templateUrl: "views/Pages/page-new.view.client.html",
+                controller: "NewPageController",
+                controllerAs: "model"
             })
             .when("/user/:userId/website/:websiteId/page/:pid", {
-                templateUrl: "views/Pages/page-edit.view.client.html"
+                templateUrl: "views/Pages/page-edit.view.client.html",
+                controller: "EditPageController",
+                controllerAs: "model"
             })
             .when("/user/:userId/website/:websiteId/page/:pid/widget", {
-                templateUrl: "views/Widget/widget-list.view.client.html"
+                templateUrl: "views/Widget/widget-list.view.client.html",
+                controller: "WidgetListController",
+                controllerAs: "model"
             })
             .when("/user/:userId/website/:websiteId/page/:pid/widget/new", {
-                templateUrl: "views/Widget/widget-chooser.view.client.html"
+                templateUrl: "views/Widget/widget-chooser.view.client.html",
+                controller: "NewWidgetController",
+                controllerAs: "model"
             })
             .when("/user/:userId/website/:websiteId/page/:pid/widget/:wgid", {
-                templateUrl: "views/widget/widget-edit.view.client.html"
+                templateUrl: "views/widget/widget-edit.view.client.html",
+                controller: "EditWidgetController",
+                controllerAs: "model"
             })
             .otherwise({
                 redirectTo: "/login"
