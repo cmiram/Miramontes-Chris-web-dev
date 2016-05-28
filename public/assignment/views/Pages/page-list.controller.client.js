4b5c519c54c99e6897f206d3ee5ef10ff06afbff
Chris Miramontes <nyyankeeschris@gmail.com>
2016-05-28 19:39:15 -0400

    added controller logic to page-list

--- public/assignment/views/Pages/page-list.controller.client.js
+++ public/assignment/views/Pages/page-list.controller.client.js
@@ -3,7 +3,14 @@
         .module("WebAppMaker")
         .controller("PageListController", PageListController);
 
-    function PageListController($location, PageService) {
+    function PageListController($location, $routeParams, PageService) {
         var vm = this;
+
+        function init() {
+            vm.userId = $routeParams.userId;
+            vm.websiteId = $routeParams.websiteId;
+            vm.pages = PageService.findPageByWebsiteId(vm.websiteId);
+        }
+        init();
     }
 })();
