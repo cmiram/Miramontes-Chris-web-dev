b14a56b7a5ac603fa20842c8d9eb0d9ce289fdf5
Chris Miramontes <nyyankeeschris@gmail.com>
2016-05-28 17:17:55 -0400

    updated profile html to user controller logic.

--- public/assignment/views/User/profile.controller.client.js
+++ public/assignment/views/User/profile.controller.client.js
@@ -3,7 +3,25 @@
         .module("WebAppMaker")
         .controller("ProfileController", ProfileController);
 
-    function ProfileController($location, UserService) {
+    function ProfileController($routeParams, UserService) {
         var vm = this;
+        var userId = $routeParams["id"];
+        vm.updateUser = updateUser;
+        
+        function init() {
+            vm.user = UserService.findUserById(userId);
+        }
+        init();
+
+        function updateUser(user) {
+            var result = UserService.updateUser(userId, user);
+
+            if(result) {
+                vm.success = "User successfully updated";
+            }
+            else {
+                vm.error = "User not found";
+            }
+        }
     }
 })();
