078a25e4768985afe824671d987f287c4c996456
Chris Miramontes <nyyankeeschris@gmail.com>
2016-05-29 15:20:27 -0400

    removed logging statement

--- public/assignment/views/Widget/widget-list.controller.client.js
+++ public/assignment/views/Widget/widget-list.controller.client.js
@@ -13,7 +13,6 @@
             vm.websiteId = $routeParams.websiteId;
             vm.pageId = $routeParams.pid;
             vm.widgets = WidgetService.findWidgetsByPageId(vm.pageId);
-            console.log(vm.widgets);
         }
         init();
 
