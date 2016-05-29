2acf55f14138e06af0b93c15abb0f980a9e5571d
Chris Miramontes <nyyankeeschris@gmail.com>
2016-05-28 23:19:35 -0400

    fixed youtube delete button.

--- public/assignment/views/Widget/widget-edit.controller.client.js
+++ public/assignment/views/Widget/widget-edit.controller.client.js
@@ -14,7 +14,6 @@
             vm.pageId = $routeParams.pid;
             vm.widgetId = $routeParams.wgid;
             vm.widget = JSON.parse(JSON.stringify(WidgetService.findWidgetsById(vm.widgetId)));
-            console.log(vm.widget);
         }
         init();
         
