5ea1529730f1a6f3d434c505ac60af18d9a059ce
Chris Miramontes <nyyankeeschris@gmail.com>
2016-06-04 18:02:14 -0400

    implemented crud operations in user service

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -14,26 +14,78 @@ module.exports = function(app) {
     app.delete("/api/user/:userId", deleteUser);
 
     function createUser(req, res) {
+        var newUser = req.body;
 
+        for(var i in users) {
+            if(users[i].username === newUser.username) {
+                res.status(400).send("Username "  + newUser.username + " is already being used");
+                return;
+            }
+        }
+
+        newUser._id = (new Date()).getTime().toString();
+        users.push(newUser);
+        res.json(newUser);
     }
 
-    function findUserByUsername(req, res) {
-
+    function findUserByUsername(username, res) {
+        for(var u in users) {
+            if(users[u].username === username) {
+                res.send(users[u]);
+                return;
+            }
+        }
+        res.send(403);
     }
 
-    function findUserByCredentials(req, res) {
-
+    function findUserByCredentials(username, password, res) {
+        for(var u in users) {
+            if(users[u].username === username && users[u].password === password) {
+                res.send(users[u]);
+                return;
+            }
+        }
+        res.send({});
     }
 
     function findUserById(req, res) {
+        var id = req.params.userId;
 
+        for(var u in users) {
+            if(users[u]._id === id) {
+                res.send(users[u]);
+                return;
+            }
+        }
+        res.send({});
     }
 
     function updateUser(req, res) {
+        var id = req.params.userId;
+        var newUser = req.body;
 
+        for(var u in users) {
+            if(users[u]._id === id) {
+                users[u].firstName = newUser.firstname;
+                users[u].lastName = newUser.lastName;
+                users[u].email = newUser.email;
+                res.send(200);
+                return;
+            }
+        }
+        res.status(400).send("User with ID: " + id + " not found");
     }
 
     function deleteUser(req, res) {
+        var id = req.params.userId;
 
+        for(var u in users) {
+            if(users[u]._id === id) {
+                users.splice(u, 1);
+                res.send(200);
+                return
+            }
+        }
+        res.status(400).send("User with ID: " + id + " not found");
     }
 };
