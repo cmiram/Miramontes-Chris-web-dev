3babcba7549194c085daa40218bcf3cc764cf28f
Chris Miramontes <nyyankeeschris@gmail.com>
2016-06-04 19:21:08 -0400

    implemented crud ops for website logic

--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -16,22 +16,63 @@ module.exports = function(app) {
     app.delete("/api/website/:websiteId", deleteWebsite);
     
     function createWebsite(req, res) {
-        
+        var newWebsite = req.body;
+
+        newWebsite._id = (new Date()).getTime().toString();
+        newWebsite.developerId = req.params.userId;
+        websites.push(newWebsite);
+        res.json(newWebsite);
     }
 
     function findAllWebsitesForUser(req, res) {
+        var userId = req.params.userId;
+        var sites = [];
 
+        for(var i in websites) {
+            if(websites[i].developerId === userId) {
+                sites.push(websites[i]);
+            }
+        }
+        res.json(sites);
     }
 
     function findWebsiteById(req, res) {
+        var id = req.params.websiteId;
 
+        for(var i in websites) {
+            if(websites[i]._id === id) {
+                res.send(websites[i]);
+                return;
+            }
+        }
+        res.status(403);
     }
 
     function updateWebsite(req, res) {
+        var id = req.params.websiteId;
+        var newWebsite = req.body;
 
+        for(var i in websites) {
+            if(websites[i]._id === id) {
+                websites[i].name = newWebsite.name;
+                websites[i].description = newWebsite.description;
+                res.send(200);
+                return;
+            }
+        }
+        res.status(403);
     }
 
     function deleteWebsite(req, res) {
+        var id = req.params.websiteId;
 
+        for(var i in websites) {
+            if(websites[i]._id === id) {
+                websites.splice(i, 1);
+                res.send(200);
+                return;
+            }
+        }
+        res.send(403);
     }
 };
