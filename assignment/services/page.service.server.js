4bd5d4495e7c9c8bfe2af68c42cfac0dfb85feff
Chris Miramontes <nyyankeeschris@gmail.com>
2016-06-04 19:43:09 -0400

    added crud ops to page and widget services'

--- assignment/services/page.service.server.js
+++ assignment/services/page.service.server.js
@@ -13,22 +13,64 @@ module.exports = function(app) {
     app.delete("/api/page/:pageId", deletePage);
     
     function createPage(req, res) {
+        var newPage = req.body;
         
+        newPage._id = (new Date()).getTime().toString();
+        newPage.websiteId = req.params.websiteId;
+        pages.push(newPage);
+        res.json(newPage);
     }
 
     function findAllPagesForWebsite(req, res) {
-
+        var id = req.params.websiteId;
+        var result = [];
+        
+        for(var i in pages) {
+            if(pages[i].websiteId === id) {
+                result.push(pages[i]);
+            }
+        }
+        
+        res.json(result);
     }
 
     function findPageById(req, res) {
-
+        var id = req.params.pageId;
+        
+        for(var i in pages) {
+            if(pages[i]._id === id) {
+                res.send(pages[i]);
+                return;
+            }
+        }
+        res.status(403);
     }
 
     function updatePage(req, res) {
-
+        var id = req.params.pageId;
+        var newPage = req.body;
+        
+        for(var i in pages) {
+            if(pages[i]._id === id) {
+                pages[i].name = newPage.name;
+                pages[i].title = newPage.title;
+                res.send(200);
+                return;
+            }
+        }
+        res.send(403);
     }
 
     function deletePage(req, res) {
-
+        var id = req.params.pageId;
+        
+        for(var i in pages) {
+            if(pages[i]._id === id) {
+                pages.splice(i, 1);
+                res.send(200);
+                return;
+            }
+        }
+        res.status(403);
     }
 };
--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-    
+
     var widgets = [
         { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
         { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
@@ -19,22 +19,63 @@ module.exports = function(app) {
     app.delete("/api/widget/:widgetId", deleteWidget);
 
     function createWidget(req,res) {
-
+        var newWidget = req.body;
+        var pageId = req.params.pageId;
+        newWidget._id = (new Date()).getTime().toString();
+        newWidget.pageId = pageId;
+        widgets.push(newWidget);
+        res.json(newWidget);
     }
 
     function findAllWidgetsForPage(req,res) {
+        var id = req.params.pageId;
+        var result = [];
 
+        for(var i in pages) {
+            if(pages[i].pageId = id) {
+                result.push(pages[i]);
+            }
+        }
+        res.json(result);
     }
 
     function findWidgetById(req,res) {
+        var id = req.params.widgetId;
 
+        for(var i in widgets) {
+            if(widgets[i]._id === id) {
+                res.send(widgets[i]);
+                return;
+            }
+        }
+        res.status(403);
     }
 
     function updateWidget(req,res) {
+        var newWidget = req.body;
+        var id = req.params.widgetId;
 
+        for(var i in widgets) {
+            if(widgets[i]._id === id) {
+                newWidget._id = id;
+                widgets[i] = newWidget;
+                res.send(newWidget);
+                return;
+            }
+        }
+        res.status(403);
     }
 
     function deleteWidget(req,res) {
+        var id = req.params.widgetId;
 
+        for(var i in widgets) {
+            if(widgets[i]._id === id) {
+                widgets.splice(i, 1);
+                res.send(200);
+                return;
+            }
+        }
+        res.status(403);
     }
 };
