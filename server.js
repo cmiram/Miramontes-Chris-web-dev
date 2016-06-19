5ee9fcff2fb87664a56ff05f3c3f2d6ac243b558
Chris Miramontes <nyyankeeschris@gmail.com>
2016-06-19 18:47:21 -0400

    added logging of mongo connection string for openshift debugging

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ else {
     connectionString = 'mongodb://127.0.0.1:27017/webdev';
 }
 
+console.log(connectionString);
 mongoose.connect(connectionString);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
