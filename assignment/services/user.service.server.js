12c61c7972d39788d84a10b056e68683580e6dbb
Chris Miramontes <nyyankeeschris@gmail.com>
2016-06-19 23:25:20 -0400

    refactored facebook callback for openshift

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -29,8 +29,8 @@ module.exports = function(app, models) {
     app.get("/auth/facebook", passport.authenticate('facebook', {scope : 'email' }));
     app.get("/auth/facebook/callback",
         passport.authenticate('facebook', {
-            successRedirect : '/assignment/index.html#/profile/',
-            failureRedirect : '#/login'
+            successRedirect : '/assignment/index.html#/profile',
+            failureRedirect : '/assignment/index.html#/login'
         }));
 
     var userModel = models.userModel;
